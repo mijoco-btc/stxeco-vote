79c0a56aafe82d7ef834f90d0d92fe5e780b8a04
mike <mjoecohen@gmail.com>
2025-06-13 08:46:15 +0100

    outstanding changes - switching for results based on localhost - block height fix

--- src/lib/components/all-voters/ProposalStageUpdate.svelte
+++ src/lib/components/all-voters/ProposalStageUpdate.svelte
@@ -6,44 +6,50 @@
 	import { onMount } from 'svelte';
 	import { isProposedPreVoting, isVoting } from '$lib/proposals';
 
-	export let proposal:VotingEventProposeProposal;
+	export let proposal: VotingEventProposeProposal;
 
 	$: endBitcoinBlock = () => {
-		const currentHeight = (proposal.submissionContract.indexOf('008') > -1) ? $sessionStore.stacksInfo.stacks_tip_height : $sessionStore.stacksInfo?.burn_block_height || 0
+		const currentHeight =
+			proposal.submissionContract.indexOf('008') > -1
+				? $sessionStore.stacksInfo.stacks_tip_height
+				: $sessionStore.stacksInfo?.burn_block_height || 0;
 		return proposal.proposalData.burnEndHeight - currentHeight;
-	}
+	};
 
 	$: startsInBitcoinBlock = () => {
-		const currentHeight = (proposal.submissionContract.indexOf('008') > -1) ? $sessionStore.stacksInfo.stacks_tip_height : $sessionStore.stacksInfo?.burn_block_height || 0
-		return proposal.proposalData.burnStartHeight - (currentHeight);
-	}
+		const currentHeight =
+			proposal.submissionContract.indexOf('008') > -1
+				? $sessionStore.stacksInfo.stacks_tip_height
+				: $sessionStore.stacksInfo?.burn_block_height || 0;
+		return proposal.proposalData.burnStartHeight - currentHeight;
+	};
 
-	onMount(async () => {
-	})
-
+	onMount(async () => {});
 </script>
 
 <div class="inline-block pt-2 pb-1 px-6 rounded-2xl border border-[#131416]/[12%]">
 	<div class="mb-1 flex items-center gap-2">
 		<span class="w-2 h-2 rounded-full bg-bloodorange"></span>
 		<p class="font-mono text-xs uppercase tracking-wider text-bloodorange">
-		{#if isProposedPreVoting(proposal) }
-			Voting starts in {fmtNumber(startsInBitcoinBlock())} blocks
-		{:else}
-			{#if isVoting(proposal)}
-			Vote in progress
+			{#if isProposedPreVoting(proposal)}
+				Voting starts in {fmtNumber(startsInBitcoinBlock())} blocks
+			{:else if isVoting(proposal)}
+				Vote in progress
 			{:else}
-			Voting closed
+				Voting closed
 			{/if}
-		{/if}
 		</p>
 	</div>
 	{#if isVoting(proposal)}
-	<div class="font-mono text-[#131416] text-xs uppercase tracking-wider">Ends in {fmtNumber(endBitcoinBlock())} bitcoin blocks</div>
-	<div class="mt-1 font-mono text-[#131416] text-xs uppercase tracking-wider"><Countdown scaleFactor={1} endBlock={endBitcoinBlock()} /></div>
+		<div class="font-mono text-[#131416] text-xs uppercase tracking-wider">
+			Ends in {fmtNumber(endBitcoinBlock())} bitcoin blocks
+		</div>
+		<div class="mt-1 font-mono text-[#131416] text-xs uppercase tracking-wider">
+			<Countdown scaleFactor={1} endBlock={endBitcoinBlock()} />
+		</div>
 	{:else}
-	<div class="mt-1 font-mono text-[#131416] text-xs uppercase tracking-wider">
-		<Countdown scaleFactor={1} endBlock={startsInBitcoinBlock()} />
-	</div>
+		<div class="mt-1 font-mono text-[#131416] text-xs uppercase tracking-wider">
+			<Countdown scaleFactor={1} endBlock={startsInBitcoinBlock()} />
+		</div>
 	{/if}
 </div>
